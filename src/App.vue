e197c1f06f6b947e45ce90f1370df2b6b7e34086
zkeyword <zjx1247225@163.com>
2016-03-11 17:49:41 +0800

    添加组件

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,24 @@
 <style lang="less">
-@import 'less/style.less';
+    @import 'less/style.less';
 </style>
 
 <template>
     <div class="page page-current">
-
-        <nav class="bar bar-tab" v-if="isIndex" >
-            <a class="tab-item" external v-link="{ path: '/home', activeClass: 'active'}">
-                <span class="icon icon-home"></span>
-                <span class="tab-label">首页</span>
-            </a>
-            <a class="tab-item" external  v-link="{ path: '/msg', activeClass: 'active' }">
-                <span class="icon icon-message"></span>
-                <span class="tab-label">消息</span>
-            </a>
-            <a class="tab-item" external v-link="{ path: '/user', activeClass: 'active' }">
-                <span class="icon icon-me"></span>
-                <span class="tab-label">我的</span>
-            </a>
-        </nav>
-
         <router-view keep-alive></router-view>
+        <nv-footer isShow="true"></nv-footer>
     </div>
 </template>
 
 <script>
 module.exports = {
-    data :function() {
+    data() {
         return {
             authenticating: false,
             isIndex:true
         }
+    },
+    components:{
+        'nvFooter': require('./components/footer.vue')
     }
 }
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,30 @@
+<style lang="less">
+
+</style>
+
+<template>
+    <nav class="bar bar-tab" v-if="isShow" >
+        <a class="tab-item" external v-link="{ path: '/home', activeClass: 'active'}">
+            <span class="icon icon-home"></span>
+            <span class="tab-label">首页</span>
+        </a>
+        <a class="tab-item" external  v-link="{ path: '/msg', activeClass: 'active' }">
+            <span class="icon icon-message"></span>
+            <span class="tab-label">消息</span>
+        </a>
+        <a class="tab-item" external v-link="{ path: '/user', activeClass: 'active' }">
+            <span class="icon icon-me"></span>
+            <span class="tab-label">我的</span>
+        </a>
+    </nav>
+</template>
+
+<script>
+    export default {
+        replace:true,
+        props: ['isShow'],
+        ready: function () {
+            console.log(this.isShow)
+        }
+    }
+</script>
